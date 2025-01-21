e85d3140489666cd11b1ccdc874b32281ecc6c21
mateu.yabar <mateu.yabar@itb.cat>
2025-01-21 12:08:51 +0100

    ex

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -1,17 +1,26 @@
 package cat.itb.m78.exercices
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import cat.itb.m78.exercices.stateless.ContactApp
+import cat.itb.m78.exercices.stateless.HelloWorld
+import cat.itb.m78.exercices.stateless.Welcome
 import cat.itb.m78.exercices.theme.AppTheme
+import cat.itb.m78.exercices.viewmodel.CounterScreen
 import org.jetbrains.compose.reload.DevelopmentEntryPoint
 
 @Composable
 internal fun App() = AppTheme {
-    Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
-    }
+    CounterScreen()
 }
+
+
