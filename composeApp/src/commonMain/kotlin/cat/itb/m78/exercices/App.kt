839891a00a2be45861f15fb3458b077ac70e145b
mateu.yabar <mateu.yabar@itb.cat>
2025-02-11 09:38:25 +0100

    TicTacToe

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -1,26 +1,15 @@
 package cat.itb.m78.exercices
 
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import cat.itb.m78.exercices.stateless.MainNavigation
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
-import cat.itb.m78.exercices.stateless.ContactApp
-import cat.itb.m78.exercices.stateless.HelloWorld
-import cat.itb.m78.exercices.stateless.Welcome
+import cat.itb.m78.exercices.navigation.tictactoe.TicTacToeScreen
 import cat.itb.m78.exercices.theme.AppTheme
-import cat.itb.m78.exercices.viewmodel.CounterScreen
-import org.jetbrains.compose.reload.DevelopmentEntryPoint
+import cat.itb.m78.exercices.viewmodel.ShoppingListScreen
 
 @Composable
 internal fun App() = AppTheme {
-    CounterScreen()
+    TicTacToeScreen()
 }
 
 
