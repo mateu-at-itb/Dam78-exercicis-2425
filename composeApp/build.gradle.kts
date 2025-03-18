d5bfc1d4eff67048bcb6d3aa2e6e08eb8b1343b1
mateu.yabar <mateu.yabar@itb.cat>
2025-03-18 09:53:34 +0100

    Apis

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -55,6 +55,12 @@ kotlin {
             implementation(libs.androidx.navigation.composee)
             implementation(libs.kotlinx.serialization.json)
             implementation(libs.kotlinx.datetime)
+            implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.2")
+            implementation("com.russhwolf:multiplatform-settings-no-arg:1.3.0")
+            implementation("com.russhwolf:multiplatform-settings-serialization:1.3.0")
+            implementation("io.coil-kt.coil3:coil-compose:3.1.0")
+            implementation("io.coil-kt.coil3:coil-network-ktor3:3.1.0")
+
         }
 
         commonTest.dependencies {
--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -4,13 +4,15 @@ package cat.itb.m78.exercices
 import cat.itb.m78.exercices.stateless.MainNavigation
 import androidx.compose.runtime.*
 import cat.itb.m78.exercices.navigation.tictactoe.TicTacToeScreen
+import cat.itb.m78.exercices.p2.countries.CountriesScreen
+import cat.itb.m78.exercices.p2.jokes.JokesScreen
 import cat.itb.m78.exercices.theme.AppTheme
 import cat.itb.m78.exercices.trivial.CountDownScreen
 import cat.itb.m78.exercices.viewmodel.ShoppingListScreen
 
 @Composable
 internal fun App() = AppTheme {
-    CountDownScreen()
+    CountriesScreen()
 }
 
 
